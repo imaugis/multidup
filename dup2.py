fa128fa3a99e69b8e5524613e086054e3db93c34
thierry <thierry.escola@imaugis.com>
2014-10-03 17:53:46 +0200

    03 10 2014

--- dup2.py
+++ dup2.py
@@ -13,6 +13,7 @@ on impose à la destination d'avoir le même UUID que l'original pour faciliter 
 """
 
 import sys,os,re,commands
+from copy import deepcopy
 
 entree="/dev/sdb"
 sorties=["/dev/sdc"]
@@ -31,54 +32,81 @@ class Partition:
 		self.filesytem=''
 		self.uuid=''
 
-		parts = re.compile("^\D+([0-9]*).*start= *([0-9]*).*size= *([0-9]*).*Id= *([0-9]*),? ?([a-zA-Z]*)?").search(part)
+		parts = re.compile(r"^\D+([0-9]*).*start= *([0-9]*).*size= *([0-9]*).*Id= *([0-9]*),? ?([a-zA-Z]*)?").search(part)
 		self.npart,self.start,self.size,self.Id,self.bootable=parts.groups()
+		self.npart=int(self.npart)
+		self.start=int(self.start)
+		self.size =int(self.size)
 
 		blkid = commands.getoutput("blkid %s" % part)	# on lit l'UUID et le système de fichier
 		if blkid:
-			rex=re.compile("UUID=\"([^\"]*)\" TYPE=\"([^\"]*)\"")
+			rex=re.compile(r"UUID=\"([^\"]*)\" TYPE=\"([^\"]*)\"")
 			resultat=rex.search(blkid)
 			self.uuid,self.filesytem=resultat.groups()
 
 	def taille(self):
 		return self.size		# retourne la taille de la partition
 
+	def sfdisk_conv(self,taille_cyl):
+		print self.size, type(self.size)
+		""" convertit en format compatible avec sfdisk en entree pour pouvoir créer les partitions sur le Disque destination """
+		s=",{},{}".format(self.size/taille_cyl if self.size!=0 else '','S' if self.Id=='82' else 'L')
+		if self.bootable=='bootable':
+			s+=',*'
+		s+='\n'
+		return s
+
 	def __repr__(self):
 		return 'Partition %s, start=%8s, size=%8s, Id=%2s, filesytem=%6s%s, UUID=%s' % (self.npart, self.start, self.size, self.Id, self.filesytem, ', bootable' if self.bootable else '          ', self.uuid)
 
 
 class Disque:
-	def lit_disque(self,disque):
-		self.device=disque 			# /dev/sdX
-		self.nbre_secteurs=0
-		self.nbre_cylindres=0
-		#self.taille_secteur=512
-		self.nbre_sect_piste=0
-		self.nbre_tetes=0
-		self.liste_part=[]
-		if disque != '':
-			s=commands.getoutput("fdisk -l %s" % disque)
+	def lit_disque(self,disk):
+		if disk != '':
+			s=commands.getoutput("fdisk -l %s" % disk)
 			for l in s.split('\n'):
 				if l[:1].isdigit():
-					rex=re.compile("^(\d+)\D+(\d+)\D+(\d+)\D+(\d+)")
+					rex=re.compile(r"^(\d+)\D+(\d+)\D+(\d+)\D+(\d+)")
 					resultat=rex.search(l)
 					self.nbre_tetes,self.nbre_sect_piste,self.nbre_cylindres,self.nbre_secteurs=resultat.groups()
+					self.nbre_tetes     =int(self.nbre_tetes)
+					self.nbre_sect_piste=int(self.nbre_sect_piste)
+					self.nbre_cylindres =int(self.nbre_cylindres)
+					self.nbre_secteurs  =int(self.nbre_secteurs)
+					self.taille_cylindre=self.nbre_sect_piste * self.nbre_tetes
 					break
 
-	def __init_(self,disque):
-		lit_disque(disque)
-		if disque != '':
-			sfdisk_output = commands.getoutput("sfdisk -d %s" % disque)	# on lit la table de partition du disque
+	def __init__(self,disk,origin=None):
+		if disk=='':
+			raise ValueError("erreur de paramètre disk")
+
+		self.device=disk 			# /dev/sdX
+		self.nbre_secteurs=0		# nbre de secteurs du disque
+		self.nbre_cylindres=0		# nbre de cylindres
+		#self.taille_secteur=512
+		self.nbre_sect_piste=0		# nbre de secteurs par piste
+		self.nbre_tetes=0
+		self.taille_cylindre=0
+		self.liste_part=[]
+
+		self.lit_disque(disk)
+		if origin == None:
+			sfdisk_output = commands.getoutput("sfdisk -d %s" % disk)	# on lit la table de partition du disque
 			for line in sfdisk_output.split("\n"):			# on explore ligne par ligne
 				if line.startswith("/"):					# si la ligne commence par un / on doit avoir un /dev/sd???
 					p=Partition(line)
-					if p.taille() != '0':					# si la partition n'est pas vide
+					if p.taille() != 0:						# si la partition n'est pas vide
 						self.liste_part.append(p)
+		else :
+			self.liste_part=deepcopy(origin.liste_part)		# recopie de la liste de partition du disque d'origine
+			self.liste_part[-1].size=0						# on annule la taille de la dernière partition, ce qui permettra d'étendre cette partition autant que nécessaire
 
-	def copie_parts(self,disque):
-		self.liste_part=[]
-		for l in
-
+	def sfdisk_conv(self):
+		""" convertit en format compatible avec sfdisk en entree pour pouvoir créer les partitions sur le Disque destination """
+		s=''
+		for p in self.liste_part:
+			s+=p.sfdisk_conv(self.taille_cylindre)
+		return s
 
 	def __repr__(self):
 		s=''
@@ -92,8 +120,9 @@ class Disque:
 				s)
 
 def main():
-	d=Disque(entree)
-	print(d)
+	i=Disque(entree)
+	o=Disque(sorties[0],i)
+	print o.device
 
 if __name__ == '__main__':
 	main()
